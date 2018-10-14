e4f38807fe5205393bdf806fb131ec71f8de81a9
ignacio <ignaciomenchaca666@gmail.com>
2018-10-14 18:23:13 -0500

    Textfields de usuario

--- src/principal/agregarUsuario.java
+++ src/principal/agregarUsuario.java
@@ -19,13 +19,22 @@ public class agregarUsuario extends JDialogMethods {
 
     public agregarUsuario(Frame owner, boolean modal) {
         super(owner, modal);
-        addWindow(null, 400,400,"", false, this);
+        addWindow(null, 400,420,"", false, this);
+
         addLabel(lbNombre, 10,10,100,30, this);
-        addTextField(txtNombre, 10, 40, 200,30, "Nombre...", this);
         addLabel(lbApellidoPaterno, 10,70,150,30, this);
-        addTextField(txtApellidoPaterno, 10, 100, 200, 30, "Apellido paterno", this);
         addLabel(lbApellidoMaterno, 10, 130, 150, 30, this);
+        addLabel(lbCorreo, 10, 190, 150, 30, this);
+        addLabel(lbDireccion, 10, 250, 150, 30, this);
+        addLabel(lbTelefono, 10, 310, 150, 30, this);
+
+        addTextField(txtNombre, 10, 40, 200,30, "Nombre...", this);
+        addTextField(txtApellidoPaterno, 10, 100, 200, 30, "Apellido paterno", this);
         addTextField(txtApellidoMaterno, 10, 160, 200, 30, "Apellido materno", this);
+        addTextField(txtCorreo, 10, 220, 200, 30, "Correo", this);
+        addTextField(txtDireccion, 10, 280, 200, 30, "Dirección", this);
+        addTextField(txtTelefono, 10, 340, 200, 30, "Telefono", this);
+
         this.setVisible(true);
     }
 }
