ae472f5ecd9b67064e6b8e990e7fa730321d854f
Raftel <rafiel-okami-kurai@hotmail.com>
2018-11-07 20:57:09 -0600

    X

---
+++ src/principal/Conexion.java
@@ -0,0 +1,17 @@
+package principal;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    Connection Conexion;
+    String Servidor;
+    String Database;
+    String Usuario;
+    String Password;
+    String Url;
+
+    public Conexion(String Servidor, String Database, String Usuario, String Password){
+
+    }
+}
---
+++ src/principal/Splash.java
@@ -0,0 +1,14 @@
+package principal;
+
+
+import javax.swing.*;
+
+public class Splash extends JFrame{
+    ImageIcon iconoFondo = new ImageIcon(getClass().getResource("/imagenes/Conteo.gif"));
+    JLabel lblFondo = new JLabel(iconoFondo);
+    int x, y;
+
+    public Splash(){
+
+    }
+}
